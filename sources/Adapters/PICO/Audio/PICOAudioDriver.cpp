7db9d6a35a1017b7c4d7248c6e69e0591b853e10
democloid <democloid@gmail.com>
2023-03-05 17:38:47 -0800

    Direct DMA transfer from audio driver. ~2x performance in opt mode

--- sources/Adapters/PICO/Audio/PICOAudioDriver.cpp
+++ sources/Adapters/PICO/Audio/PICOAudioDriver.cpp
@@ -6,12 +6,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "hardware/clocks.h"
+#include "hardware/dma.h"
+#include "hardware/gpio.h"
 #include "hardware/irq.h"
-#include "pico/audio.h"
+#include "hardware/pio.h"
+#include "audio_i2s.pio.h"
 #include <math.h>
 #include <stdio.h>
 
-// #define SAMPLES_PER_BUFFER 1156 // Samples / channel
 #define SAMPLES_PER_BUFFER 256 // Samples / channel
 
 PICOAudioDriver *PICOAudioDriver::instance_ = NULL;
@@ -23,7 +26,10 @@ void PICO_sound_pause(int yes) { PICO_sound_pausei = yes; }
 // This calls comes after the call to the same function name in the pico audio
 // driver
 void __isr __time_critical_func(audio_i2s_dma_irq_handler)() {
-  PICOAudioDriver::IRQHandler();
+  if (dma_irqn_get_channel_status(PICO_AUDIO_I2S_DMA_IRQ, PICO_AUDIO_I2S_DMA)) {
+    dma_irqn_acknowledge_channel(PICO_AUDIO_I2S_DMA_IRQ, PICO_AUDIO_I2S_DMA);
+    PICOAudioDriver::IRQHandler();
+  }
 }
 
 void PICOAudioDriver::IRQHandler() { instance_->OnChunkDone(); }
@@ -32,25 +38,7 @@ PICOAudioDriver::PICOAudioDriver(AudioSettings &settings)
     : AudioDriver(settings), miniBlank_(0) {
 
   isPlaying_ = false;
-
-  spin_lock_ = spin_lock_init(spin_lock_claim_unused(true));
-
   PICO_exit = 0;
-
-  audio_format_ = {.sample_freq = 44100,
-                   .format = AUDIO_BUFFER_FORMAT_PCM_S16,
-                   .channel_count = 2};
-
-  static audio_buffer_format_t producer_format = {.format = &audio_format_,
-                                                  .sample_stride = 4};
-
-  ap_ = audio_new_producer_pool(&producer_format, 3,
-                                SAMPLES_PER_BUFFER); // todo correct size
-
-  i2s_config_ = {.data_pin = PICO_AUDIO_I2S_DATA_PIN,
-                 .clock_pin_base = PICO_AUDIO_I2S_CLOCK_PIN_BASE,
-                 .dma_channel = 0,
-                 .pio_sm = 0};
 }
 
 PICOAudioDriver::~PICOAudioDriver() { PICO_exit = 1; }
@@ -59,32 +47,58 @@ bool PICOAudioDriver::InitDriver() {
   instance_ = this;
 
   // pico audio init
-  const audio_format_t *output_format;
+  // Setup GPIOs
+  gpio_set_function(PICO_AUDIO_I2S_DATA_PIN, GPIO_FUNC_PIOx);
+  gpio_set_function(PICO_AUDIO_I2S_CLOCK_PIN_BASE, GPIO_FUNC_PIOx);
+  gpio_set_function(PICO_AUDIO_I2S_CLOCK_PIN_BASE + 1, GPIO_FUNC_PIOx);
 
-  output_format = audio_i2s_setup(&audio_format_, &i2s_config_);
-  if (!output_format) {
-    Trace::Error("PicoAudio: Unable to open audio device");
-    return false;
-  }
+  // Claim and configure PIO0
+  pio_sm_claim(audio_pio, PICO_AUDIO_I2S_SM);
+
+  uint offset =
+      pio_add_program(audio_pio, &audio_i2s_program);
+
+  audio_i2s_program_init(audio_pio, PICO_AUDIO_I2S_SM, offset,
+                         PICO_AUDIO_I2S_DATA_PIN,
+                         PICO_AUDIO_I2S_CLOCK_PIN_BASE);
+
+  // Claim and configure DMA
+  dma_channel_claim(PICO_AUDIO_I2S_DMA);
+  dma_channel_config dma_config =
+      dma_channel_get_default_config(PICO_AUDIO_I2S_DMA);
+
+  channel_config_set_dreq(&dma_config, DREQ_PIOx_TX0 + PICO_AUDIO_I2S_SM);
+  channel_config_set_transfer_data_size(&dma_config, DMA_SIZE_32);
+  channel_config_set_read_increment(&dma_config, true);
+  dma_channel_configure(PICO_AUDIO_I2S_DMA, &dma_config,
+                        &audio_pio->txf[PICO_AUDIO_I2S_SM], // dest
+                        NULL,                               // src
+                        0,                                  // count
+                        false                               // trigger
+  );
 
   // Add our own callback func to run after the i2s irq func (priority 0x80)
-  irq_add_shared_handler(DMA_IRQ_0 + PICO_AUDIO_I2S_DMA_IRQ,
-                         audio_i2s_dma_irq_handler,
-                         PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY - 0x40);
+  irq_set_exclusive_handler(DMA_IRQ_0 + PICO_AUDIO_I2S_DMA_IRQ, audio_i2s_dma_irq_handler);
+  dma_irqn_set_channel_enabled(PICO_AUDIO_I2S_DMA_IRQ, PICO_AUDIO_I2S_DMA, true);
 
-  bool ok = audio_i2s_connect(ap_);
-  assert(ok);
-  { // initial buffer data
-    audio_buffer_t *buffer = take_audio_buffer(ap_, true);
-    int16_t *samples = (int16_t *)buffer->buffer->bytes;
-    for (uint i = 0; i < buffer->max_sample_count; i++) {
-      samples[i * 2 + 0] = 0;
-      samples[i * 2 + 1] = 0;
-    }
-    buffer->sample_count = buffer->max_sample_count;
-    give_audio_buffer(ap_, buffer);
-  }
-  audio_i2s_set_enabled(true);
+  // Set PIO frequency
+  uint32_t system_clock_frequency = clock_get_hz(clk_sys);
+  int sample_freq = 44100;
+  // This number is exactly 20000 for our 220.5MHz core freq
+  uint32_t divider =
+      system_clock_frequency * 4 / sample_freq; // avoid arithmetic overflow
+  pio_sm_set_clkdiv_int_frac(audio_pio, PICO_AUDIO_I2S_SM, divider >> 8u,
+                             divider & 0xffu);
+
+  // Create mini blank buffer for underrun
+  miniBlank_ = (char *)malloc(SAMPLES_PER_BUFFER * 2 * sizeof(short));
+  memset(miniBlank_, 0, SAMPLES_PER_BUFFER * 2 * sizeof(short));
+
+  // Enable audio
+  irq_set_enabled(DMA_IRQ_0 + PICO_AUDIO_I2S_DMA_IRQ, true);
+  dma_channel_transfer_from_buffer_now(PICO_AUDIO_I2S_DMA, miniBlank_,
+                                       SAMPLES_PER_BUFFER);
+  pio_sm_set_enabled(audio_pio, PICO_AUDIO_I2S_SM, true);
 
   volume_ = 65;
   Config *config = Config::GetInstance();
@@ -93,10 +107,6 @@ bool PICOAudioDriver::InitDriver() {
   if (volume) {
     volume_ = atoi(volume);
   }
-
-  // Create mini blank buffer for underrun
-  miniBlank_ = (char *)malloc(SAMPLES_PER_BUFFER * 2 * sizeof(short));
-  memset(miniBlank_, 0, SAMPLES_PER_BUFFER * 2 * sizeof(short));
 
   return true;
 };
@@ -109,8 +119,6 @@ void PICOAudioDriver::SetVolume(int v) {
 int PICOAudioDriver::GetVolume() { return volume_; };
 
 void PICOAudioDriver::CloseDriver() {
-  spin_lock_unclaim(spin_lock_get_num(spin_lock_));
-  audio_i2s_set_enabled(false);
   if (miniBlank_) {
     SYS_FREE(miniBlank_);
     miniBlank_ = 0;
@@ -150,36 +158,29 @@ void PICOAudioDriver::StopDriver() {
 
 void PICOAudioDriver::OnChunkDone() {
   if (isPlaying_) {
-    audio_buffer_t *buffer = take_audio_buffer(ap_, false);
-    if (buffer == NULL) {
-      printf("No buffer avail\n");
-      return;
-    }
+
+    // TODO: Only clear last buffer if last transfer finished
+
+    // clear last ring buffer
+    SYS_FREE(pool_[poolPlayPosition_].buffer_);
+    pool_[poolPlayPosition_].buffer_ = 0;
+    poolPlayPosition_ = (poolPlayPosition_ + 1) % SOUND_BUFFER_COUNT;
+
+    // Start DMA of next one
+
+    dma_channel_transfer_from_buffer_now(PICO_AUDIO_I2S_DMA,
+                                         pool_[poolPlayPosition_].buffer_,
+                                         pool_[poolPlayPosition_].size_ / 4);
+    int start = micros();
+    OnNewBufferNeeded();
+    printf("%i - Time taken on OnNewBufferNeeded(): %ius\n", micros(), micros() - start);
+
     // Process MIDI
     if (ticksBeforeMidi_) {
       ticksBeforeMidi_--;
     } else {
       MidiService::GetInstance()->Flush();
     }
-    if (buffer->max_sample_count >= pool_[poolPlayPosition_].size_ / 4) {
-      memcpy(buffer->buffer->bytes, pool_[poolPlayPosition_].buffer_,
-             pool_[poolPlayPosition_].size_);
-      buffer->sample_count = pool_[poolPlayPosition_].size_ / 4;
-      SYS_FREE(pool_[poolPlayPosition_].buffer_);
-      pool_[poolPlayPosition_].buffer_ = 0;
-      poolPlayPosition_ = (poolPlayPosition_ + 1) % SOUND_BUFFER_COUNT;
-      OnNewBufferNeeded();
-    } else {
-      memcpy(buffer->buffer->bytes, pool_[poolPlayPosition_].buffer_,
-             buffer->max_sample_count * 4);
-      buffer->sample_count = buffer->max_sample_count;
-      memmove(pool_[poolPlayPosition_].buffer_,
-              pool_[poolPlayPosition_].buffer_ + (buffer->max_sample_count * 4),
-              pool_[poolPlayPosition_].size_ - (buffer->max_sample_count * 4));
-      pool_[poolPlayPosition_].size_ -= buffer->max_sample_count * 4;
-    }
-    give_audio_buffer(ap_, buffer);
-    onAudioBufferTick();
   }
 }
 
--- sources/Adapters/PICO/Audio/PICOAudioDriver.h
+++ sources/Adapters/PICO/Audio/PICOAudioDriver.h
@@ -1,17 +1,20 @@
 #ifndef _PICOAUDIO_DRIVER_H_
 #define _PICOAUDIO_DRIVER_H_
 
-#define PICO_AUDIO_I2S_CLOCK_PIN_BASE 26
-#define PICO_AUDIO_I2S_DATA_PIN 28
-#define AUDIO_I2S
-
 #include "Foundation/T_Singleton.h"
 #include "Services/Audio/AudioDriver.h"
-#include "pico/audio_i2s.h"
-
 #include "System/Process/Process.h"
 
-// #define SOUND_BUFFER_COUNT 25
+#define PICO_AUDIO_I2S_CLOCK_PIN_BASE 26
+#define PICO_AUDIO_I2S_DATA_PIN 28
+#define PICO_AUDIO_I2S_PIO 0
+#define audio_pio __CONCAT(pio, PICO_AUDIO_I2S_PIO)
+#define GPIO_FUNC_PIOx __CONCAT(GPIO_FUNC_PIO, PICO_AUDIO_I2S_PIO)
+#define DREQ_PIOx_TX0 __CONCAT(__CONCAT(DREQ_PIO, PICO_AUDIO_I2S_PIO), _TX0)
+#define PICO_AUDIO_I2S_SM 0
+#define PICO_AUDIO_I2S_DMA 0
+#define PICO_AUDIO_I2S_DMA_IRQ 0
+#define AUDIO_I2S
 
 class PICOAudioDriver : public AudioDriver {
 public:
@@ -43,10 +46,5 @@ private:
   int ticksBeforeMidi_;
   uint32_t startTime_;
 
-  // pico driver specific
-  audio_i2s_config_t i2s_config_;
-  audio_format_t audio_format_;
-  audio_buffer_pool_t *ap_;
-  spin_lock_t *spin_lock_;
 };
 #endif
