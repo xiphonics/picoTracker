979da1005f71dbce77168a147ad26c76d56c0ebe
Maksim Lin <maks@manichord.com>
2024-03-12 21:20:09 +1100

    read input events from usb serial uart

--- sources/Adapters/picoTracker/system/input.cpp
+++ sources/Adapters/picoTracker/system/input.cpp
@@ -1,3 +1,12 @@
 #include "input.h"
+#include <stdio.h>
 
-uint16_t scanKeys() { return (~gpio_get_all() & 0x0001FF00) >> 8; }
+uint16_t scanKeys() {
+  // use param of 0 for immediate timeout and check for return of 255 for no
+  // char read result ref: https://forums.raspberrypi.com/viewtopic.php?t=303964
+  char c = getchar_timeout_us(0);
+  if (c != 255) {
+    return c;
+  }
+  return (~gpio_get_all() & 0x0001FF00) >> 8;
+}
