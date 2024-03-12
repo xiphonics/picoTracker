1038ae9f9bb95eb834c52d90b55e6d7f39d31810
Maksim Lin <maks@manichord.com>
2024-03-13 10:01:46 +1100

    put using USB for user input behind define flag

--- sources/Adapters/picoTracker/system/input.cpp
+++ sources/Adapters/picoTracker/system/input.cpp
@@ -2,11 +2,13 @@
 #include <stdio.h>
 
 uint16_t scanKeys() {
+#ifdef USB_UI
   // use param of 0 for immediate timeout and check for return of 255 for no
   // char read result ref: https://forums.raspberrypi.com/viewtopic.php?t=303964
   char c = getchar_timeout_us(0);
   if (c != 255) {
     return c;
   }
+#endif
   return (~gpio_get_all() & 0x0001FF00) >> 8;
 }
