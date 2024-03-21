ee5c99bde1e2b221f49fd912b723ee317bca9ba6
Maksim Lin <maks@manichord.com>
2024-03-21 19:18:21 +1100

    use midi style split data across 2 chars

--- sources/Adapters/picoTracker/system/input.cpp
+++ sources/Adapters/picoTracker/system/input.cpp
@@ -11,8 +11,22 @@ uint16_t scanKeys() {
   // group by the code below.
   // That bit mask is documented in the KEYPAD_BITS enum.
   char c = getchar_timeout_us(0);
-  if (c != 255) {
-    return c;
+  if (c != 0xFF) {
+    // how to encode a 9bit bitmask into a 7bit char? (8th bit is not usable
+    // because 0xFF means no result) answer: use MIDI style encoding, if bitmask
+    // > 0x40 then set bit 7 to 1 to indicate 2 chars needed and then the next
+    // char contains the remaining 3 bits
+    int16_t mask = c & 0x3F;
+    if (c & 0xFE) {
+      // get remaining 3 bits from reading the next char:
+      c = getchar_timeout_us(0);
+      if (c != 0xFF) {
+        mask += (c & 0x7) << 6;
+      } else {
+        // TODO: error missing follow up char!
+      }
+    }
+    return mask;
   }
 #endif
   return (~gpio_get_all() & 0x0001FF00) >> 8;
