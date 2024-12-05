b99d0d1567353266b7cec11ac36b7d1c48239607
Maksim Lin <maks@manichord.com>
2024-12-05 20:02:02 +1100

    update manufacturer

--- sources/Adapters/picoTracker/usb/usb_descriptors.c
+++ sources/Adapters/picoTracker/usb/usb_descriptors.c
@@ -149,7 +149,7 @@ uint8_t const *tud_descriptor_configuration_cb(uint8_t index) {
 // array of pointer to string descriptors
 char const *string_desc_arr[] = {
     (const char[]){0x09, 0x04}, // 0: is supported language is English (0x0409)
-    "xiphonics",                // 1: Manufacturer
+    "xiphonics, inc",           // 1: Manufacturer
     "PicoTracker",              // 2: Product
     "123456",                   // 3: Serials, should use chip ID
     "TinyUSB CDC",              // 4: CDC Interface
