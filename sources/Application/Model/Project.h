3a48ec2dd270f0c13a33d29ffa14cf2bcd89e04f
Maksim Lin <maks@manichord.com>
2025-02-18 11:30:10 +1100

    bump version number for release

--- sources/Application/Model/Project.h
+++ sources/Application/Model/Project.h
@@ -10,7 +10,7 @@
 #include "Foundation/Variables/VariableContainer.h"
 #include "Song.h"
 
-#define PROJECT_NUMBER "2.0-BETA8A"
+#define PROJECT_NUMBER "2.0-RC1"
 #define PROJECT_RELEASE "r"
 // BUILD_COUNT define comes from BuildNumber.h
 
