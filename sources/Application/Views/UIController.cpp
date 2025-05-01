94f38c9e29c654aeb30793a302ecca068336cdbe
Maksim Lin <maks@manichord.com>
2025-05-01 21:27:40 +1000

    mute and solo midi instruments per channel

--- sources/Application/Views/UIController.cpp
+++ sources/Application/Views/UIController.cpp
@@ -1,5 +1,7 @@
 #include "UIController.h"
 
+#include "Application/Instruments/MidiInstrument.h"
+#include "Application/Model/Project.h"
 #include "Application/Player/Player.h"
 
 UIController::UIController(){};
@@ -32,27 +34,94 @@ void UIController::UnMuteAll() {
 void UIController::ToggleMute(int from, int to) {
 
   Player *player = Player::GetInstance();
+  Project *project = player->GetProject();
+  
   for (int i = from; i < to + 1; i++) {
-    bool muted = player->IsChannelMuted(i);
-    player->SetChannelMute(i, !muted);
+    bool wasMuted = player->IsChannelMuted(i);
+    bool willBeMuted = !wasMuted;
+    
+    // Check if there's a currently playing instrument on this channel
+    // We'll only handle MIDI instruments that are currently playing
+    I_Instrument *instrument = nullptr;
+    if (player->IsChannelPlaying(i)) {
+      // Get the instrument from the channel
+      instrument = player->GetProject()->GetInstrumentBank()->GetInstrument(i % MAX_INSTRUMENT_COUNT);
+    }
+    
+    // Set the mute state in the player
+    player->SetChannelMute(i, willBeMuted);
+    
+    // If it's a MIDI instrument, handle the mute change
+    if (instrument && instrument->GetType() == IT_MIDI) {
+      MidiInstrument *midiInstrument = static_cast<MidiInstrument *>(instrument);
+      midiInstrument->HandleMuteChange(i, willBeMuted);
+    }
   };
 };
 
 void UIController::SwitchSoloMode(int from, int to, bool soloing) {
 
   Player *player = Player::GetInstance();
+  Project *project = player->GetProject();
 
   // If not in solo mode, we solo current channel or selection
-
   if (soloing) {
-
+    // Store current mute states and set new mute states
     for (int i = 0; i < SONG_CHANNEL_COUNT; i++) {
       soloMask_[i] = player->IsChannelMuted(i);
-      player->SetChannelMute(i, (i < from) || (i > to));
+      bool willBeMuted = (i < from) || (i > to);
+      
+      // If the mute state is changing, handle it for MIDI instruments
+      if (soloMask_[i] != willBeMuted) {
+        // Set the mute state in the player
+        player->SetChannelMute(i, willBeMuted);
+        
+        // Check if there's a currently playing instrument on this channel
+        // We'll only handle MIDI instruments that are currently playing
+        I_Instrument *instrument = nullptr;
+        if (player->IsChannelPlaying(i)) {
+          // Get the instrument from the channel
+          instrument = player->GetProject()->GetInstrumentBank()->GetInstrument(i % MAX_INSTRUMENT_COUNT);
+        }
+            
+        // If it's a MIDI instrument, handle the mute change
+        if (instrument && instrument->GetType() == IT_MIDI) {
+          MidiInstrument *midiInstrument = static_cast<MidiInstrument *>(instrument);
+          midiInstrument->HandleMuteChange(i, willBeMuted);
+        }
+      } else {
+        // No change in mute state, just set it
+        player->SetChannelMute(i, willBeMuted);
+      }
     };
   } else {
+    // Restore previous mute states
     for (int i = 0; i < SONG_CHANNEL_COUNT; i++) {
-      player->SetChannelMute(i, soloMask_[i]);
+      bool wasMuted = player->IsChannelMuted(i);
+      bool willBeMuted = soloMask_[i];
+      
+      // If the mute state is changing, handle it for MIDI instruments
+      if (wasMuted != willBeMuted) {
+        // Set the mute state in the player
+        player->SetChannelMute(i, willBeMuted);
+        
+        // Check if there's a currently playing instrument on this channel
+        // We'll only handle MIDI instruments that are currently playing
+        I_Instrument *instrument = nullptr;
+        if (player->IsChannelPlaying(i)) {
+          // Get the instrument from the channel
+          instrument = player->GetProject()->GetInstrumentBank()->GetInstrument(i % MAX_INSTRUMENT_COUNT);
+        }
+            
+        // If it's a MIDI instrument, handle the mute change
+        if (instrument && instrument->GetType() == IT_MIDI) {
+          MidiInstrument *midiInstrument = static_cast<MidiInstrument *>(instrument);
+          midiInstrument->HandleMuteChange(i, willBeMuted);
+        }
+      } else {
+        // No change in mute state, just set it
+        player->SetChannelMute(i, willBeMuted);
+      }
     };
   }
 };
