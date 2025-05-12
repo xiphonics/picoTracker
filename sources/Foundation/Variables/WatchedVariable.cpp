fab6410621996543f58f7d8351ad27ea02d14470
Maksim Lin <maks@manichord.com>
2025-05-12 12:48:30 +1000

    add string watchedvariables, then use for projectname
    also removes the prev hacky global that exposed projectname for use in mixerservice

--- sources/Foundation/Variables/WatchedVariable.cpp
+++ sources/Foundation/Variables/WatchedVariable.cpp
@@ -15,13 +15,19 @@ WatchedVariable::WatchedVariable(FourCC id, const char *const *list, int size,
                                  int index)
     : Variable(id, list, size, index) {
   updating_ = false;
+}
+
+WatchedVariable::WatchedVariable(FourCC id, const char *value)
+    : Variable(id, value) {
+  updating_ = false;
 };
 
 void WatchedVariable::onChange() {
   if (!updating_ && enabled_) {
     updating_ = true;
     SetChanged();
-    NotifyObservers();
+    // Cast the FourCC value to I_ObservableData* as done in other parts of the codebase
+    NotifyObservers((I_ObservableData *)(uintptr_t)id_);
     updating_ = false;
   }
 };
