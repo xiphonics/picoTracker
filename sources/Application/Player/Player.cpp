8796150f41e433ba87a44ffe4e04c8ab5104f513
Maksim Lin <maks@manichord.com>
2025-09-12 16:01:39 +1000

    Dont try to take mixer lock when stop is called (#780)
    * dont try to take mixer lock when stop is called
    
    allows code that already holds mixer lock to call stop without it trying
    to take the lock
    
    * add strong docs on not using lock for Player::Stop()

--- sources/Application/Player/Player.cpp
+++ sources/Application/Player/Player.cpp
@@ -202,9 +202,15 @@ void Player::Start(PlayMode mode, bool forceSongMode, MixerServiceMode msmMode,
   mixer_.Unlock();
 }
 
-void Player::Stop() {
+// DANGER! WILL ROBINSON DANGER!
+// set nolock to true ONLY when calling from code that ALREADY hold the mixer
+// lock currently that only place that can occur is when processing the STP
+// command
+void Player::Stop(bool nolock) {
 
-  mixer_.Lock();
+  if (!nolock) {
+    mixer_.Lock();
+  }
 
   for (int i = 0; i < SONG_CHANNEL_COUNT; i++) {
     mixer_.StopChannel(i);
@@ -218,7 +224,9 @@ void Player::Stop() {
   PlayerEvent pe(PET_STOP);
   NotifyObservers(&pe);
 
-  mixer_.Unlock();
+  if (!nolock) {
+    mixer_.Unlock();
+  }
 }
 
 const char *Player::GetPlayedNote(int channel) {
@@ -589,7 +597,6 @@ void Player::Update(Observable &o, I_ObservableData *d) {
     now_ = system->GetClock();
 
     // Notify refresh
-
     PlayerEvent pe(PET_UPDATE, 0);
     SetChanged();
     NotifyObservers(&pe);
@@ -699,7 +706,7 @@ bool Player::ProcessChannelCommand(int channel, FourCC cmd, ushort param) {
   case FourCC::InstrumentCommandStop: {
     switch (GetSequencerMode()) {
     case SM_SONG:
-      Stop();
+      Stop(true);
       break;
     case SM_LIVE:
       //            QueueChannel(channel,QM_CHAINSTOP,0) ;
