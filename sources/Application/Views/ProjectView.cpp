9c83fe813e888a3d2678b667335a3b0db26835b1
Maksim Lin <maks@manichord.com>
2025-10-30 17:19:17 +1100

    Add "compact samples" (#897)
    * add "compact samples, fix compact instruments bug
    compact instruments was not respecting user not confirming dialog.
    also removed unnecessary bool flag
    * better naming of remove unused... buttons
    * only remove unused instruments not also samplesnow only remove unused instruments, not unused samples at same time, as we now have separate button for that.

--- sources/Application/Views/ProjectView.cpp
+++ sources/Application/Views/ProjectView.cpp
@@ -81,7 +81,15 @@ static void SaveAsOverwriteCallback(View &v, ModalView &dialog) {
 }
 
 static void PurgeCallback(View &v, ModalView &dialog) {
-  ((ProjectView &)v).OnPurgeInstruments(dialog.GetReturnCode() == MBL_YES);
+  if (dialog.GetReturnCode() == MBL_YES) {
+    ((ProjectView &)v).OnPurge();
+  }
+};
+
+static void PurgeInstrumentsCallback(View &v, ModalView &dialog) {
+  if (dialog.GetReturnCode() == MBL_YES) {
+    ((ProjectView &)v).OnPurgeInstruments();
+  }
 };
 
 static void RenderStopCallback(View &v, ModalView &dialog) {
@@ -146,7 +154,13 @@ ProjectView::ProjectView(GUIWindow &w, ViewData *data) : FieldView(w, data) {
   (*actionField_.rbegin()).AddObserver(*this);
 
   position._y += 1;
-  actionField_.emplace_back("Compact Instruments",
+  actionField_.emplace_back("Remove Unused Samples", FourCC::ActionPurge,
+                            position);
+  fieldList_.insert(fieldList_.end(), &(*actionField_.rbegin()));
+  (*actionField_.rbegin()).AddObserver(*this);
+
+  position._y += 1;
+  actionField_.emplace_back("Remove Unused Instruments",
                             FourCC::ActionPurgeInstrument, position);
   fieldList_.insert(fieldList_.end(), &(*actionField_.rbegin()));
   (*actionField_.rbegin()).AddObserver(*this);
@@ -298,13 +312,16 @@ void ProjectView::Update(Observable &, I_ObservableData *data) {
   Player *player = Player::GetInstance();
 
   switch (fourcc) {
-  case FourCC::ActionPurge:
-    project_->Purge();
-    break;
-  case FourCC::ActionPurgeInstrument: {
+  case FourCC::ActionPurge: {
     MessageBox *mb =
         new MessageBox(*this, "Remove unused samples?", MBBF_YES | MBBF_NO);
     DoModal(mb, PurgeCallback);
+    break;
+  }
+  case FourCC::ActionPurgeInstrument: {
+    MessageBox *mb =
+        new MessageBox(*this, "Remove unused instruments?", MBBF_YES | MBBF_NO);
+    DoModal(mb, PurgeInstrumentsCallback);
     break;
   }
   case FourCC::ActionRandomName: {
@@ -444,9 +461,9 @@ void ProjectView::Update(Observable &, I_ObservableData *data) {
   isDirty_ = true;
 };
 
-void ProjectView::OnPurgeInstruments(bool removeFromDisk) {
-  project_->PurgeInstruments(removeFromDisk);
-};
+void ProjectView::OnPurge() { project_->PurgeSamples(); };
+
+void ProjectView::OnPurgeInstruments() { project_->PurgeInstruments(); };
 
 void ProjectView::OnQuit() {
   ViewEvent ve(VET_QUIT_APP);
