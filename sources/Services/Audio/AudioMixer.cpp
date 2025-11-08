c8bb403d2d484623ab9781dc0327ec2066878aad
democloid <democloid@gmail.com>
2025-11-08 15:52:07 -0800

    improve mixing performance

--- sources/Services/Audio/AudioMixer.cpp
+++ sources/Services/Audio/AudioMixer.cpp
@@ -53,16 +53,45 @@ bool AudioMixer::Render(fixed *buffer, int samplecount) {
         fixed *dst = buffer;
         fixed *src = renderBuffer_;
         int count = samplecount * 2;
-        while (count--) {
-          *dst += *src;
-          dst++;
-          src++;
+
+        /* Manually unrolling the loop gives a 25% performance increase (from
+         * 6500 cycles for 800 samples to 5100 cycles. This is due to being able
+         * to schedule independent loads simultanously, reduce load latency by
+         * loading independent loads instead of immediately executing a
+         * dependent action (add) having to wait for the load to complete.
+         * Potentially also reducing the load comparison overhead 16
+         * instructions in the unroll gives the best performance, 8 gave ~17%
+         * improvement.
+         */
+        int i = 0;
+        for (; i + 16 <= count; i += 16) {
+          dst[i + 0] += src[i + 0];
+          dst[i + 1] += src[i + 1];
+          dst[i + 2] += src[i + 2];
+          dst[i + 3] += src[i + 3];
+          dst[i + 4] += src[i + 4];
+          dst[i + 5] += src[i + 5];
+          dst[i + 6] += src[i + 6];
+          dst[i + 7] += src[i + 7];
+          dst[i + 8] += src[i + 8];
+          dst[i + 9] += src[i + 9];
+          dst[i + 10] += src[i + 10];
+          dst[i + 11] += src[i + 11];
+          dst[i + 12] += src[i + 12];
+          dst[i + 13] += src[i + 13];
+          dst[i + 14] += src[i + 14];
+          dst[i + 15] += src[i + 15];
         }
+        for (; i < count; ++i)
+          dst[i] += src[i];
       }
     }
   }
 
   //  Apply volume to mix of all of this instances "sub" audiomixers
+  // TODO (democloid): This is wildly inefficient, doing this loop takes 4 - 5
+  // times the time it takes a mix loop above. Some tests show that at least
+  // double performance is not hard to achieve
   if (gotData) {
     fixed *c = buffer;
     if (volume_ != i2fp(1)) {
