0ebdc0a2638742bb62c43d341967ec44437f7ae0
nILS <github@schickt.de>
2026-02-17 16:30:40 +0100

    adds memory pool and singe fileIndexList

--- sources/System/FileSystem/FileSystem.h
+++ sources/System/FileSystem/FileSystem.h
@@ -36,7 +36,8 @@ public:
   virtual bool read(int index, void *data) {
     return false;
   } // Default implementation
-  virtual void list(etl::ivector<int> *fileIndexes, const char *filter, bool subDirOnly, bool sorted=false) = 0;
+  virtual void list(etl::ivector<int> *fileIndexes, const char *filter,
+                    bool subDirOnly, bool sorted = false) = 0;
   virtual void getFileName(int index, char *name, int length) = 0;
   virtual PicoFileType getFileType(int index) = 0;
   virtual bool isParentRoot() = 0;
