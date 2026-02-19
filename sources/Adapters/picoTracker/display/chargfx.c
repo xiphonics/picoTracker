8dfc5443b6e0c7232c6ee2ee7e5dbaaefbc81191
Maksim Lin <maks@manichord.com>
2026-02-19 16:12:06 +1100

    fix position of waveform on sample editor screen
    needs to set position per each type of lcd controller on pico

--- sources/Adapters/picoTracker/display/chargfx.c
+++ sources/Adapters/picoTracker/display/chargfx.c
@@ -121,17 +121,21 @@ void chargfx_fill_rect(uint8_t color_index, uint16_t x, uint16_t y,
     return;
   }
   if (x + width > ILI9341_TFTHEIGHT) {
-    width = ILI9341_TFTHEIGHT;
+    width = ILI9341_TFTHEIGHT - x;
   }
   if (y + height > ILI9341_TFTWIDTH) {
-    height = ILI9341_TFTWIDTH;
+    height = ILI9341_TFTWIDTH - y;
   }
 
   // display_x is from right hand edge and since the picoTracker LCD is mounted
   // rotated 90deg clockwise, the LCDs "physical height" is actually visually
   // speaking the width
   uint16_t display_x = ILI9341_TFTHEIGHT - x - width;
+#ifdef LCD_ST7789
   uint16_t display_y = y;
+#else
+  uint16_t display_y = ILI9341_TFTWIDTH - y - height;
+#endif
   uint16_t display_w = width;
   uint16_t display_h = height;
 
