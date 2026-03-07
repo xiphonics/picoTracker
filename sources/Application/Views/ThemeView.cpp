4eb175503c9663336d30200a2075758f4699f921
nILS <github@schickt.de>
2026-03-07 22:46:13 +0100

    fixes theme switching display issues ++

--- sources/Application/Views/ThemeView.cpp
+++ sources/Application/Views/ThemeView.cpp
@@ -279,6 +279,12 @@ void ThemeView::syncColorComponentVars(Variable *colorVar) {
     uint32_t componentValue =
         (colorValue >> entry.shift) & static_cast<uint32_t>(0xFF);
     entry.componentVar->SetInt(static_cast<int>(componentValue), false);
+
+    if (entry.colorVar->GetID() == FourCC::VarBGColor) {
+      // If the background or foreground color changed, we need to force a
+      // redraw to update all the colors on the screen
+      _forceRedraw = true;
+    }
   }
 }
 
@@ -408,8 +414,8 @@ void ThemeView::handleThemeExport() {
   auto fs = FileSystem::GetInstance();
   if (fs->exists(pathBuffer)) {
     // Theme exists, ask for confirmation
-    MessageBox *mb = MessageBox::Create(
-        *this, "Theme already exists. Overwrite?", MBBF_YES | MBBF_NO);
+    MessageBox *mb = MessageBox::Create(*this, "Theme already exists.",
+                                        "Overwrite?", MBBF_YES | MBBF_NO);
 
     DoModal(mb, ModalViewCallback::create<ThemeView,
                                           &ThemeView::onConfirmThemeOverwrite>(
